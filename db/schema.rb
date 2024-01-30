4b769b7c4c618b148cde4d917b4affd9a63c401e
mik3ymikes <mikey.henleys@gmail.com>
2024-01-30 14:10:36 -0600

    completed has-and -belongs to many association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_30_193806) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_30_200341) do
   create_table "appointments", force: :cascade do |t|
     t.integer "physician_id", null: false
     t.integer "patient_id", null: false
@@ -18,6 +18,25 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_30_193806) do
     t.datetime "updated_at", null: false
     t.index ["patient_id"], name: "index_appointments_on_patient_id"
     t.index ["physician_id"], name: "index_appointments_on_physician_id"
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "authors_books", id: false, force: :cascade do |t|
+    t.integer "author_id", null: false
+    t.integer "book_id", null: false
+    t.index ["author_id", "book_id"], name: "index_authors_books_on_author_id_and_book_id"
+    t.index ["book_id", "author_id"], name: "index_authors_books_on_book_id_and_author_id"
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "patients", force: :cascade do |t|
